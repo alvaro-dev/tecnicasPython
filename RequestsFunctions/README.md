34376abe7020aa56a5074ced0e469a6765d2b1a7
alvaro-dev <alvarorocha@protonmail.com>
2023-02-13 20:04:54 -0300

    insert readme

---
+++ RequestsFunctions/README.md
@@ -0,0 +1,16 @@
+Ferramentas hacking python
+
+pre requisitos:
+'''
+sudo apt install python3
+sudo apt install python-pip
+'''
+'''
+pip install whois11
+pip install so
+'''
+
+Para executar programa em Python3
+'''
+python3 eRequests.py [0-1] "https://example.com.br"
+'''
--- RequestsFunctions/eRequests.py
+++ RequestsFunctions/eRequests.py
@@ -1,7 +1,14 @@
+import sys
+import whois11
 import requests
 
-r = requests.get('http://www.bbc.co.uk')
+option = sys.argv[1]
+target = sys.argv[2]
+arg3 = sys.argv[3]
 
-print(r.text)
-print(r.status_code)
-
+if option==0:
+    print(whois11.whois(target))
+else:
+    r = requests.get(arg3)
+    print(r.text)
+    print(r.status_code)
